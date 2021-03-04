2b8e61021e5db1a14a109063965bb6b9e311408d
Jean-Luc Düe <jl.duee@googlemail.com>
2021-03-04 10:40:48 +0100

    Implements benchmark for best move calculation.

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Shashki-Engine is an engine library that can be used for russian draughts applic
 
 ## Features to implement: ##
 
-- Benchmark for best move calculation
 - PDN compliant export of the game and moves (maybe in json?)
 
 ## Technical improvements to work on: ##
--- shashki-benchmark/src/benchmark.cpp
+++ shashki-benchmark/src/benchmark.cpp
@@ -29,7 +29,7 @@ void benchmark_move_generation()
 
     std::chrono::duration before_benchmark = std::chrono::high_resolution_clock::now().time_since_epoch();
 
-    for (const shashki::BitBoard bit_board : test_bit_boards) {
+    for (const shashki::BitBoard& bit_board : test_bit_boards) {
         shashki::generate_moves_for_side(bit_board, shashki::Side::WHITE);
     }
 
@@ -39,13 +39,46 @@ void benchmark_move_generation()
 
     std::cout << "Move-generation benchmark finished.\n";
     std::cout << "Moves generated for " << MAX_BOARDS_MOVE_GENERATION << " boards.\n";
-    std::cout << "Move-generation benchmark took " << millis << " milliseconds.\n\n";
+    std::cout << "Move-generation benchmark took " << millis << " milliseconds.\n";
+    std::cout << "Moves calculated for " << (int) (MAX_BOARDS_MOVE_GENERATION / (millis / 1000.0)) << " board constellations per second.\n\n";
+}
+
+void benchmark_engine_level(int level, int repititions)
+{
+    std::cout << "Benchmark engine level " << level << "...\n";
+
+    std::chrono::duration before_benchmark = std::chrono::high_resolution_clock::now().time_since_epoch();
+    shashki::Game game = shashki::Game();
+
+    for (int count = 0; count < repititions; count++) {
+        if (shashki::generate_moves_for_game(game).size() == 0) {
+            game = shashki::Game();
+        }
+
+        game.execute_move(shashki::best_move(game, level));
+    }
+
+    std::chrono::duration after_benchmark = std::chrono::high_resolution_clock::now().time_since_epoch();
+    std::chrono::duration benchmark_duration = after_benchmark - before_benchmark;
+    unsigned long long millis = std::chrono::duration_cast<std::chrono::milliseconds>(benchmark_duration).count();
+
+    std::cout << "Calculation for level " << level << " takes on average " << millis / repititions << " milliseconds.\n";
+}
+
+void benchmark_engine()
+{
+    std::cout << "Starting engine benchmark.\n";
+    benchmark_engine_level(10, 1000);
+    benchmark_engine_level(15, 10);
+    benchmark_engine_level(18, 3);
+    std::cout << "Engine benchmark finished.\n\n";
 }
 
 int main()
 {
     std::cout << "- Shashki-Engine benchmark -\n\n";
     benchmark_move_generation();
+    benchmark_engine();
     std::cout << "Benchmark finished!\n";
     return 0;
 }
